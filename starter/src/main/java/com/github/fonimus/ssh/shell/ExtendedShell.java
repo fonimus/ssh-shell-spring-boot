a6cf82ede0126bb1ec6dfbb2d583c88cddc72ea4
Francois Onimus <francois.onimus@gmail.com>
2019-03-23 13:10:18 +0100

    Combine nested if statement

--- starter/src/main/java/com/github/fonimus/ssh/shell/ExtendedShell.java
+++ starter/src/main/java/com/github/fonimus/ssh/shell/ExtendedShell.java
@@ -46,32 +46,30 @@ public class ExtendedShell
         List<String> words = input.words();
         Object toReturn = super.evaluate(new ExtendedInput(input));
         SshContext ctx = SSH_THREAD_CONTEXT.get();
-        if (ctx != null) {
-            if (isKeyCharInList(words)) {
-                List<Integer> indexes = IntStream.range(0, words.size()).filter(i -> KEY_CHARS.contains(words.get(i))).boxed().collect(Collectors.toList());
-                List<PostProcessorObject> postProcessors = new ArrayList<>();
-                for (Integer index : indexes) {
-                    if (words.size() > index + 1) {
-                        String keyChar = words.get(index);
-                        if (keyChar.equals(PIPE)) {
-                            String postProcessorName = words.get(index + 1);
-                            int currentIndex = 2;
-                            String word = words.size() > index + currentIndex ? words.get(index + currentIndex) : null;
-                            List<String> params = new ArrayList<>();
-                            while (word != null && !KEY_CHARS.contains(word)) {
-                                params.add(word);
-                                currentIndex++;
-                                word = words.size() > index + currentIndex ? words.get(index + currentIndex) : null;
-                            }
-                            postProcessors.add(new PostProcessorObject(postProcessorName, params));
-                        } else if (keyChar.equals(ARROW)) {
-                            postProcessors.add(new PostProcessorObject(SavePostProcessor.SAVE, Collections.singletonList(words.get(index + 1))));
+        if (ctx != null && isKeyCharInList(words)) {
+            List<Integer> indexes = IntStream.range(0, words.size()).filter(i -> KEY_CHARS.contains(words.get(i))).boxed().collect(Collectors.toList());
+            List<PostProcessorObject> postProcessors = new ArrayList<>();
+            for (Integer index : indexes) {
+                if (words.size() > index + 1) {
+                    String keyChar = words.get(index);
+                    if (keyChar.equals(PIPE)) {
+                        String postProcessorName = words.get(index + 1);
+                        int currentIndex = 2;
+                        String word = words.size() > index + currentIndex ? words.get(index + currentIndex) : null;
+                        List<String> params = new ArrayList<>();
+                        while (word != null && !KEY_CHARS.contains(word)) {
+                            params.add(word);
+                            currentIndex++;
+                            word = words.size() > index + currentIndex ? words.get(index + currentIndex) : null;
                         }
+                        postProcessors.add(new PostProcessorObject(postProcessorName, params));
+                    } else if (keyChar.equals(ARROW)) {
+                        postProcessors.add(new PostProcessorObject(SavePostProcessor.SAVE, Collections.singletonList(words.get(index + 1))));
                     }
                 }
-                LOGGER.debug("Found {} post processors", postProcessors.size());
-                ctx.setPostProcessorsList(postProcessors);
             }
+            LOGGER.debug("Found {} post processors", postProcessors.size());
+            ctx.setPostProcessorsList(postProcessors);
         }
         return toReturn;
     }
